ca2c8e65241bd786fe6f86fb8f23c9a00e9c0ab4
Johnatan Osterloh <johnatanosterloh@gmail.com>
2020-04-07 11:52:16 -0300

    Desafio 02: conceitos do Node.js aplicado no GoStack 11 da RocketSeat. Abordando os conceitos básicos do Node no back-end, definição de rotas, estrutura de dados com JSON, HTTP codes, métodos HTTP e tipos de parâmetros (query params, route params e request body)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,87 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title } = request.query;
+  const result = title
+    ? repositories.filter(repositorie => repositorie.title.includes(title))
+    : repositories;
+
+  return response.json(result);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, techs} = request.body;
+  const repositorie = {
+    id: uuid(),
+    title,
+    url: 'https://github.com/osterloh/GoStack_Level01_ChallengeNode',
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({ error: 'Repositorie not found.'});
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes,
+  }
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositorieIndex < 0){
+    return res.status(400).json({ error: 'Repositorie not found.'});
+  }
+
+  repositories.splice(repositorieIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({ error: 'Repositorie not found.'});
+  }
+
+  result = repositories[repositorieIndex].likes;
+
+  const repositorie = {
+    id,
+    title: repositories[repositorieIndex].title,
+    url: repositories[repositorieIndex].url,
+    techs: repositories[repositorieIndex].techs,
+    likes: result + 1,
+  }
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie);
 });
 
 module.exports = app;
